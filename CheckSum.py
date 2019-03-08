68593ed8c4b69d6a85d1ec21fd2d358ebf066040
Paul Wilson <pwilson802@gmail.com>
2019-03-08 22:22:27 +1100

    almost there..

--- CheckSum.py
+++ CheckSum.py
@@ -1,16 +1,31 @@
 import string
+import re
 
 def checkio(data):
-    data = data.replace(' ', '')
+    data = re.compile(r'\w').findall(data)
+#    data = data.replace(' ', '')
     res = 0
-    lettermap = {x:sum([int(i) for i in str(ord(x))]) for x in string.ascii_uppercase }
+    lettermap = {x:sum([int(i) for i in str(((ord(x)) - 48) * 2)]) for x in string.ascii_uppercase }
     nummap = {'0':0, '1':2, '2':4, '3': 6, '4':8, '5':1, '6':3, '7':5, '8':7, '9':9}
     all_mappings = {**lettermap, **nummap}
     print(all_mappings)
     for num, x in enumerate(list(reversed(data))):
+        print(x)
         if num % 2 == 0:
             res += all_mappings[x]
+            print(all_mappings[x])
         else:
-            res += int(x)
+            if x in string.ascii_uppercase:
+                print(ord(x) - 48)
+                res += (ord(x) - 48)
+            else:
+                print(x)
+                res += int(x)
     print(res)
-    return [res % 10, res]
+    if res % 10 == 0:
+        check_num = '0'
+    else:
+        check_num = str(10 - (res % 10))
+    print(check_num)
+
+    return [check_num, res]
