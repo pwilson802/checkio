2d5bc8305d2597dcfdaf5d1df0837ebd9f14c11f
Paul Wilson <pwilson802@gmail.com>
2019-03-09 22:46:45 +1100

    checksum working

--- CheckSum.py
+++ CheckSum.py
@@ -2,8 +2,7 @@ import string
 import re
 
 def checkio(data):
-    data = re.compile(r'\w').findall(data)
-#    data = data.replace(' ', '')
+    data = re.compile(r'[A-Za-z0-9]').findall(data)
     res = 0
     lettermap = {x:sum([int(i) for i in str(((ord(x)) - 48) * 2)]) for x in string.ascii_uppercase }
     nummap = {'0':0, '1':2, '2':4, '3': 6, '4':8, '5':1, '6':3, '7':5, '8':7, '9':9}
@@ -13,19 +12,25 @@ def checkio(data):
         print(x)
         if num % 2 == 0:
             res += all_mappings[x]
-            print(all_mappings[x])
         else:
             if x in string.ascii_uppercase:
-                print(ord(x) - 48)
                 res += (ord(x) - 48)
             else:
-                print(x)
                 res += int(x)
-    print(res)
     if res % 10 == 0:
         check_num = '0'
     else:
         check_num = str(10 - (res % 10))
-    print(check_num)
-
     return [check_num, res]
+
+
+#These "asserts" using only for self-checking and not necessary for auto-testing
+if __name__ == '__main__':
+    assert (checkio("799 273 9871") == ["3", 67]), "First Test"
+    assert (checkio("139-MT") == ["8", 52]), "Second Test"
+    assert (checkio("123") == ["0", 10]), "Test for zero"
+    assert (checkio("999_999") == ["6", 54]), "Third Test"
+    assert (checkio("+61 820 9231 55") == ["3", 37]), "Fourth Test"
+    assert (checkio("VQ/WEWF/NY/8U") == ["9", 201]), "Fifth Test"
+
+    print("OK, done!")
